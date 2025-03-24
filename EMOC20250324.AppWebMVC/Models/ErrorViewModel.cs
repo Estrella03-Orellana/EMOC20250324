76bd34daca4c23a2d29eb9977ac3404facb7ea61
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-03-24 07:33:35 -0600

    Agregar archivos de proyecto.

---
+++ EMOC20250324.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMOC20250324.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
